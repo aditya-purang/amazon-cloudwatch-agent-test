102ed3e63447a61e31f0ec3c158502c6cadda0d4
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-04-30 17:12:07 +0100

    update cluster name

--- terraform/eks/daemon/awsneuron/main.tf
+++ terraform/eks/daemon/awsneuron/main.tf
@@ -18,7 +18,7 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 resource "aws_eks_cluster" "this" {
-  name     = "cwagent-eks-integ-neuron-2"
+  name     = "cwagent-eks-integ-neuron-3"
   role_arn = module.basic_components.role_arn
   version  = var.k8s_version
   enabled_cluster_log_types = [
