02c426f800b62b2bdffcb64290e4eb74ede5ee79
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-04-30 16:02:12 +0100

    change cluster name

--- terraform/eks/daemon/awsneuron/main.tf
+++ terraform/eks/daemon/awsneuron/main.tf
@@ -18,7 +18,7 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 resource "aws_eks_cluster" "this" {
-  name     = "cwagent-eks-integ-neuron"
+  name     = "cwagent-eks-integ-neuron-1"
   role_arn = module.basic_components.role_arn
   version  = var.k8s_version
   enabled_cluster_log_types = [
