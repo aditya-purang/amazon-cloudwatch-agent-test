204f2310a14d28c7137f72f793cc92f859e1aeb3
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-08-07 15:52:22 +0100

    make agent bake time as 7 min

--- test/awsneuron/neuron_metrics_test.go
+++ test/awsneuron/neuron_metrics_test.go
@@ -110,7 +110,7 @@ func (t *AwsNeuronTestRunner) GetAgentConfigFileName() string {
 }
 
 func (t *AwsNeuronTestRunner) GetAgentRunDuration() time.Duration {
-	return 6 * time.Minute
+	return 7 * time.Minute
 }
 
 func (t *AwsNeuronTestRunner) GetMeasuredMetrics() []string {
